f14ce58f230850274d976ee6ecd21f89f3b01a9b
stelios <bpstelios10@hotmail.com>
2018-09-03 01:20:29 +0300

    [master] Adds new component: view single bike from admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
+import { ViewRegistrationComponent } from './components/view-registration/view-registration.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'admin/view/:id', component: ViewRegistrationComponent },
   { path: 'admin', component: AdminComponent }
 ];
 
