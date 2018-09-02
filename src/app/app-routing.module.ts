f29e6f708a865d1b91ae63a70c0d2554e7dd2715
stelios <bpstelios10@hotmail.com>
2018-09-02 23:48:37 +0300

    [master] Introduces routing and exports /admin url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AdminComponent } from './components/admin/admin.component';
+
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
