1ecde0aacd7b4f31f02443dd38b3f76dfaf57ada
stelios <bpstelios10@hotmail.com>
2018-09-03 01:08:31 +0300

    [master] Adds new component: home/registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'admin', component: AdminComponent }
 ];
 
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { BikeService } from '../../services/bike.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  models: string[] = [
+    'Globo MTB 28 Full Suspension',
+    'Globo Carbon Fiber Race Series',
+    'Globo Time Trial Blade'
+  ];
+  bikeform: FormGroup;
+  validMessage = '';
+
+  constructor(private bikeService: BikeService) { }
+
+  ngOnInit() {
+    this.bikeform = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      model: new FormControl('', Validators.required),
+      serialNumber: new FormControl('', Validators.required),
+      purchasePrice: new FormControl('', Validators.required),
+      purchaseDate: new FormControl('', Validators.required),
+      contract: new FormControl()
+    });
+  }
+
+  submitRegistration() {
+    if (this.bikeform.valid) {
+      this.validMessage = 'Your bike registration has been submitted. Thank you!';
+      this.bikeService.createBikeRegistration(this.bikeform.value).subscribe(
+        data => {
+          this.bikeform.reset();
+          return true;
+        },
+        error => {
+          return Observable.throw(error);
+        }
+      );
+    } else {
+        this.validMessage = 'Please fill out the form properly before submitting!';
+    }
+  }
+
+}
